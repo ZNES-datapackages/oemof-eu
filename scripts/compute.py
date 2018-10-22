148fb0528467c67add0ada64f1262227254f8028
simnh <simon.hilpert@uni-flensburg.de>
2018-10-22 11:52:13 +0200

    Update compute file

--- scripts/compute.py
+++ scripts/compute.py
@@ -2,17 +2,18 @@
 from datapackage import Package
 
 from oemof.solph import EnergySystem, Model
-from renpass import options, renpass
-from datapackage_utilities import aggregation
+from renpass import options, renpass, postprocessing
+from datapackage_utilities import aggregation, building
 
 import pprint
 
-test = False
+test = True
 
 time = {}
 
 if test:
-    path = aggregation.temporal_clustering('datapackage.json', 3, path='/tmp', how='daily') + '/'
+    print("Clustering for temporal aggregation ... ")
+    path = aggregation.temporal_clustering('datapackage.json', 15, path='/tmp', how='daily') + '/'
 else:
     path = ''
 
@@ -26,20 +27,33 @@ time['energysystem'] = renpass.stopwatch()
 
 es._typemap = options.typemap
 
-m = Model(es) #objective_weighting=es.temporal['weighting']
+m = Model(es, objective_weighting=es.temporal['weighting'])
 time['model'] = renpass.stopwatch()
 
 m.solve('gurobi')
 time['solve'] = renpass.stopwatch()
 
 
-if False: 
+if False:
 	m.write('model.lp', io_options={'symbolic_solver_labels': True})
 
 renpass.write_results(es, m, Package(path + 'datapackage.json'),
                       **{
-                    '--output-directory':'results',
+                    '--output-directory':'results-component',
                     '--output-orient': 'component'})
+
+renpass.write_results(es, m, Package(path + 'datapackage.json'),
+                      **{
+                    '--output-directory':'results-bus',
+                    '--output-orient': 'bus'})
+
+postprocessing.connection_net_results(
+    'results-component/angus_base_scenario__temporal_cluster__daily_15/sequences/connection.csv',
+    hubs=[b + '-electricity' for b in building.get_config()['buses']])
+
+postprocessing.storage_net_results(
+    'results-component/angus_base_scenario__temporal_cluster__daily_15/sequences/storage.csv')
+
 time['write_results'] = renpass.stopwatch()
 
 
