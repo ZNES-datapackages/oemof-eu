c3df6f39664cdb09d4044361014822cd75202474
simnh <simon.hilpert@uni-flensburg.de>
2018-11-13 15:11:32 +0100

    Add offshore wind tech

--- scripts/capacity_factors_wind.py
+++ scripts/capacity_factors_wind.py
@@ -8,23 +8,35 @@ from datapackage_utilities import building
 
 config = building.get_config()
 
-filepath = building.download_data(
+on_filepath = building.download_data(
     'https://www.renewables.ninja/static/downloads/ninja_europe_wind_v1.1.zip',
     unzip_file='ninja_wind_europe_v1.1_future_longterm_national.csv')
+
+off_filepath = building.download_data(
+    'https://www.renewables.ninja/static/downloads/ninja_europe_wind_v1.1.zip',
+    unzip_file='ninja_wind_europe_v1.1_future_nearterm_on-offshore.csv')
 
 year = str(config['weather_year'])
 
 # not in ninja dataset, as new market zones? (replace by german factor)
 missing = ['LU' 'CZ' 'AT' 'CH']
+
 countries = list(set(config['buses']) - set(['LU', 'CZ', 'AT', 'CH']))
 
-raw_data = pd.read_csv(filepath, index_col=[0], parse_dates=True)
+onshore_data = pd.read_csv(on_filepath, index_col=[0], parse_dates=True)
 
-df = raw_data.loc[year][countries]
+offshore_data = pd.read_csv(off_filepath, index_col=[0], parse_dates=True)
+
+df = onshore_data.loc[year][countries]
 
 sequences_df = pd.DataFrame(index=df.index)
 
+
 for c in config['buses']:
+    # add offshore profile if country exists in offshore data columns
+    if [col for col in offshore_data.columns if c + '_OFF' in col]:
+        sequences_df['wind-off-' + c + '-profile'] = offshore_data[c + '_OFF']
+
     sequence_name = 'wind-' + c + '-profile'
     if c == 'LU':
         sequences_df[sequence_name] = df['BE'].values
--- scripts/technologies.py
+++ scripts/technologies.py
@@ -30,52 +30,61 @@ all_technologies.index = all_technologies.index.str.replace(' ', '_')
 for idx, row in all_technologies.iterrows():
     if idx in investment_technologies:
         for b in buses:
-            element_name = idx + '_' + b
-            element = dict(row)
+            if 'wind_off' in idx and b.strip('-electricity') not in [
+                'BE', 'DE', 'DK', 'FI', 'FR', 'GB', 'IE', 'IT', 'NL', 'NO',
+                'SE']:
+                pass
+            else:
+                element_name = idx + '_' + b
+                element = dict(row)
 
-            if row['type'] == 'dispatchable':
-                element.update({
-                    'capacity_cost': annuity(
-                        row['Investment in Euro/kW'], row['lifetime'], 0.07) * 1000,
-                    'capacity_potential': None,
-                    'bus': b,
-                    'marginal_cost': (
-                        carrier.loc[row['carrier']].cost + carrier.loc[row['carrier']].emission * carrier.loc['co2'].cost) / row['efficiency'],
-                    'tech': idx,
-                    'edge_parameters': json.dumps({
-                        "emmission_factor":  (carrier.loc[row['carrier']].emission /
-                                              row['efficiency'])
+                if row['type'] == 'dispatchable':
+                    element.update({
+                        'capacity_cost': annuity(
+                            row['Investment in Euro/kW'], row['lifetime'], 0.07) * 1000,
+                        'capacity_potential': None,
+                        'bus': b,
+                        'marginal_cost': (
+                            carrier.loc[row['carrier']].cost +
+                            carrier.loc[row['carrier']].emission *
+                            carrier.loc['co2'].cost) / row['efficiency'],
+                        'tech': idx,
+                        'edge_parameters': json.dumps({
+                            "emmission_factor":  (carrier.loc[row['carrier']].emission /
+                                                  row['efficiency'])
+                            })
                         })
-                    })
 
-            if row['type'] == 'volatile':
-                if 'wind' in idx:
-                    profile = 'wind-' + b.strip('-electricity') + '-profile'
-                if 'pv' in idx:
-                    profile = 'pv-' + b.strip('-electricity') + '-profile'
-                element.update({
-                    'capacity_cost': annuity(
-                        row['Investment in Euro/kW'], row['lifetime'], 0.07) * 1000,
-                    'capacity_potential': None,
-                    'bus': b,
-                    'tech': idx,
-                    'profile': profile})
+                if row['type'] == 'volatile':
+                    if 'wind_off' in idx:
+                        profile = 'wind-off-' + b.strip('-electricity') + '-profile'
+                    elif 'wind' in idx:
+                        profile = 'wind-' + b.strip('-electricity') + '-profile'
+                    elif 'pv' in idx:
+                        profile = 'pv-' + b.strip('-electricity') + '-profile'
+                    element.update({
+                        'capacity_cost': annuity(
+                            row['Investment in Euro/kW'], row['lifetime'], 0.07) * 1000,
+                        'capacity_potential': None,
+                        'bus': b,
+                        'tech': idx,
+                        'profile': profile})
 
-            if row['type'] == 'storage':
-                if 'battery' in idx:
-                    cr = 0.2
-                if 'caes' in idx:
-                    cr = 0.125
-                element.update({
-                    'bus': b,
-                    'tech': idx,
-                    'efficiency': row['efficiency'],
-                    'capacity_ratio': cr,
-                    'capacity_cost': annuity(
-                        row['Investment in Euro/kW'], row['lifetime'], 0.07) * 1000,
-                    'loss': 0})
+                if row['type'] == 'storage':
+                    if 'battery' in idx:
+                        cr = 0.2
+                    if 'caes' in idx:
+                        cr = 0.125
+                    element.update({
+                        'bus': b,
+                        'tech': idx,
+                        'efficiency': row['efficiency'],
+                        'capacity_ratio': cr,
+                        'capacity_cost': annuity(
+                            row['Investment in Euro/kW'], row['lifetime'], 0.07) * 1000,
+                        'loss': 0})
 
-            data[row['type']][element_name] = element
+                data[row['type']][element_name] = element
 
 # write elements to CSV-files
 for element_type in element_dfs:
