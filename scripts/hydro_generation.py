1a5c98ad82b1c79e5b32aa33725783a2982ceedc
simnh <simon.hilpert@uni-flensburg.de>
2018-11-28 10:01:10 +0100

    Update hydro generation

--- scripts/hydro_generation.py
+++ scripts/hydro_generation.py
@@ -8,10 +8,15 @@ from xlrd import open_workbook, XLRDError
 
 from datapackage import Package, Resource
 import pandas as pd
+from oemof.tools.economics import annuity
 
 from datapackage_utilities import building
 
 config = building.get_config()
+
+technologies = pd.DataFrame(Package('https://raw.githubusercontent.com/ZNES-datapackages/technology-cost/master/datapackage.json').get_resource('electricity').read(keyed=True))
+technologies = technologies.groupby(['year', 'tech', 'carrier']).apply(lambda x: dict(zip(x.parameter, x.value))).reset_index('carrier').apply(lambda x: dict({'carrier': x.carrier}, **x[0]), axis=1)
+technologies = technologies.loc[config['year']].to_dict()
 
 dirpath = building.download_data(
     "https://zenodo.org/record/804244/files/Hydro_Inflow.zip",
@@ -39,10 +44,8 @@ for f in files:
         x = pd.concat([x, s], axis=1)
 
 total_hydro = x.sum(axis=0) * 1000 * 0.9  # GWh to MWh # DIW p.79 Table 35 Run-of-river or reservoir 2050
-total_hydro.index = e.index.str[-2:]
+total_hydro.index = total_hydro.index.str[-2:]
 total_hydro.index.name = 'iso-country-code'
-
-
 
 
 filename = 'e-Highway2050_2050_Country_and_cluster_installed_capacities_31-03-2015.xlsx'
@@ -81,26 +84,26 @@ for country in config['regions']:
             element_name = tech + '-' + country
 
             if tech == 'ror':
-                elements[element_name] = {
+                elements[element_name] = dict({
                     'type': 'volatile',
                     'bus': country + '-electricity',
                     'profile': 0.65,
                     'tech': tech,
                     'carrier': 'hydro',
-                    'capacity': round(df.at[country, tech], 4)}
+                    'capacity': round(df.at[country, tech], 4)
+                    }, **technologies[tech])
 
             elif tech == 'phs':
                 if df.at[country, tech] > 0:
-                    elements[element_name] = {
+                    elements[element_name] = dict({
                         'type': 'storage',
                         'tech': tech,
                         'carrier': 'hydro',
                         'bus': country + '-electricity',
-                        'efficiency': 0.8,
                         'loss': 0,
                         'capacity': df.at[country, tech],
-                        'storage_capacity': df.at[country, 'PSP reservoir (GWh)'] * 1000
-                    } # to MWh
+                        'storage_capacity': df.at[country, 'PSP reservoir (GWh)'] * 1000 # to MWh
+                    }, **technologies[tech])
 
             elif tech == 'reservoir':
                 if df.at[country, tech] > 0:
@@ -110,18 +113,24 @@ for country in config['regions']:
                             float(hydro_shares.at[country, 'reservoir']))
                     else:
                         production = 0
-                    elements[element_name] = {
+                    elements[element_name] = dict({
                         'type': 'dispatchable',
                         'tech': tech,
                         'carrier': 'hydro',
                         'bus': country + '-electricity',
                         'edge_parameters': _embed(production),
                         'capacity': df.at[country, tech]
-                    }
+                    }, **technologies[tech])
 
 df = pd.DataFrame.from_dict(elements, orient='index')
 
-for element_type in techmap.values():
+#
+df['capacity_cost'] = df.apply(
+    lambda x: annuity(float(x['capacity_cost']) * 1000,
+                      float(x['lifetime']),
+                      config['wacc']), axis=1)
+
+for tech, element_type in techmap.items():
     path = building.write_elements(
-            element_type + '.csv',
+            tech + '.csv',
             df.loc[df['type'] == element_type].dropna(how='all', axis=1))
